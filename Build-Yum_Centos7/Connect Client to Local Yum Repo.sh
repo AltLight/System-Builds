96ce8ee89a8f1314df5fdc1052bcec56d1684554
AltLight <realistically.paranoid@gmail.com>
2020-05-11 13:51:32 -0500

    corrected linux to local yum server connection and config script

--- Build-Yum_Centos7/Connect Client to Local Yum Repo.sh
+++ Build-Yum_Centos7/Connect Client to Local Yum Repo.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 #
 # This script connects the client to a local yum server.
+compname=$(hostname)
 if [ -z dnsdomainname ]
 then
     read -rep $'What is the domain for this system?\nex: domain.local\n>' domain
@@ -9,7 +10,6 @@ then
     sed -i "$ a DOMAIN=\"$domain\"" /etc/sysconfig/network-scripts/*$ipint*
 else
     domain=$(dnsdomainname)
-    fqdn="$compname.$domain"    
 fi
 #
 # Move all default repo config files to archive:
@@ -19,20 +19,20 @@ sudo mv -f /etc/yum.repos.d/*.repo /etc/yum.repos.d/Archive
 # Create local repo file:
 Repo_Dir=(base centosplus extras updates)
 #
+cd /etc/yum.repos.d/
 for Dir in ${Repo_Dir[@]}
 do
     file_name="local-$Dir.repo"
     touch $file_name
-    dot_domian=".$domain"
-    echo -e "[Local-$Dir]
-    name=$Dir
-    baseurl=http://yum$dot_domain/$Dir
-    enabled=1
-    gpgcheck=0
-    " >> $file_name
-    mv $file_name /etc/yum.repos.d/$file_name
+    dot_domain=".$domain"
+    echo -e "[local-$Dir]
+name=CentOS $Dir
+baseurl=http://yum$dot_domain/$Dir/
+enabled=1
+gpgcheck=0" >> $file_name
     yum --enablerepo=local-$Dir
 done
+cd ~
 #
 # Clean Yum and update client
 sudo yum clean all
